901f9ff36c2ab08594d267702c575af54d6f28d2
nitinbairwa28 <136908048+nitinbairwa28@users.noreply.github.com>
2024-11-15 21:41:36 +0530

    Update DAY1-Array1-SecondLargest.cpp

--- DAY1-Array1-SecondLargest.cpp
+++ DAY1-Array1-SecondLargest.cpp
@@ -1,1 +1,26 @@
+// Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+#include<iostream>
+#include<vector>
+using namespace std;
+int secondlargest(vector<int>&arr,int n){
+    int largest=arr[0];
+    int slargest=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            slargest=largest;
+            largest=arr[i];
+        }
+        else if(arr[i]<largest && arr[i]>slargest){
+            slargest=arr[i];
+        }
+    }
+    return slargest;
+}
+int main(){
+ vector<int> vec={1,2,4,7,7,5};
+ int n=vec.size();
+ int result= secondlargest(vec,n);
+ cout<<"the second largest is:"<<result;
 
+}
+// output= 5

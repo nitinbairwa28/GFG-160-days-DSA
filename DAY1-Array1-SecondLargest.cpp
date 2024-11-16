3747f6e87a1733b1b878f977f21b6211d8ca4509
nitinbairwa28 <136908048+nitinbairwa28@users.noreply.github.com>
2024-11-16 20:58:58 +0530

    Update DAY1-Array1-SecondLargest.cpp

--- DAY1-Array1-SecondLargest.cpp
+++ DAY1-Array1-SecondLargest.cpp
@@ -1,26 +1,35 @@
 // Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-int secondlargest(vector<int>&arr,int n){
-    int largest=arr[0];
-    int slargest=-1;
-    for(int i=0;i<n;i++){
-        if(arr[i]>largest){
-            slargest=largest;
-            largest=arr[i];
-        }
-        else if(arr[i]<largest && arr[i]>slargest){
-            slargest=arr[i];
+
+int secondLargest( vector<int>& arr) {
+   
+
+    int largest = arr[0];
+    int slargest = -1; 
+
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] > largest) {
+            slargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > slargest && arr[i] < largest) {
+            slargest = arr[i];
         }
     }
+
+   
+
     return slargest;
 }
-int main(){
- vector<int> vec={1,2,4,7,7,5};
- int n=vec.size();
- int result= secondlargest(vec,n);
- cout<<"the second largest is:"<<result;
 
+int main() {
+    vector<int> vec = {1, 2, 4, 7, 70, 54,80};
+    int result = secondLargest(vec);
+    
+        cout << "The second largest is: " << result << endl;
+    
+    return 0;
 }
+
 // output= 5
